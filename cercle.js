04dcc65477ddb4c04343acce70197a87e76dabcf
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-08 01:14:45 +0100

    travail de style

--- cercle.js
+++ cercle.js
@@ -1,10 +1,1 @@
-function CircleArea(r) {
-    return Math.PI * (r * r);
- }
-  
- document.write("L'aire d'un cercle de rayon de 1 cm est " +CircleArea(1) +"cm2<BR>");
- document.write("L'aire d'un cercle de rayon de 5 cm est " +CircleArea(5) +"cm2<BR>");
- document.write("L'aire d'un cercle de rayon de 8 cm est " +CircleArea(8) +"cm2<BR>");
- document.write("L'aire d'un cercle de rayon de 10 cm est " +CircleArea(10) +"cm2<BR>"); 
-
-//  document.write("L'aire d'un cercle de rayon de " +r+ "cm est " +CircleArea(r) +"cm2<BR>");
+document.write("<h1>CALCUL DE L'AIRE DU CERCLE EN FONCTION DE SON RAYON</h1>");
---
+++ cercle2.js
@@ -0,0 +1,10 @@
+function CircleArea(r) {
+    return Math.PI * (r * r);
+ }
+  
+ document.write("L'aire d'un cercle de rayon de 1 cm est " +CircleArea(1) +"cm2<BR>");
+ document.write("L'aire d'un cercle de rayon de 5 cm est " +CircleArea(5) +"cm2<BR>");
+ document.write("L'aire d'un cercle de rayon de 8 cm est " +CircleArea(8) +"cm2<BR>");
+ document.write("L'aire d'un cercle de rayon de 10 cm est " +CircleArea(10) +"cm2<BR>"); 
+
+//  document.write("L'aire d'un cercle de rayon de " +r+ "cm est " +CircleArea(r) +"cm2<BR>");
