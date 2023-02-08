0dfd158867cf21845b780291e27442917297cadf
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-08 02:04:33 +0100

    commentaires pour le formulaires en js

--- cercle2.js
+++ cercle2.js
@@ -1,5 +1,52 @@
 var r = document.write("<form action='' method='get'><label for='number'>Rayon :</label><input class='r' type='number' id='number' name='number' size='10'></form>");
 
+
+// var email = document.getElementById("mail");
+
+// email.addEventListener("keyup", function (event) {
+//   if(email.validity.typeMismatch) {
+//     email.setCustomValidity("J'attends un e-mail, mon cher !");
+//   } else {
+//     email.setCustomValidity("");
+//   }
+// });
+
+
+// Il y a plusieurs façon de sélectionner un nœud DOM ; ici on récupère
+// le formulaire et le champ d'e-mail ainsi que l'élément span
+// dans lequel on placera le message d'erreur
+
+
+
+// var form  = document.getElementsByTagName('form')[0];
+// var email = document.getElementById('mail');
+// var error = document.querySelector('.error');
+
+// email.addEventListener("input", function (event) {
+//   // Chaque fois que l'utilisateur saisit quelque chose
+//   // on vérifie la validité du champ e-mail.
+//   if (email.validity.valid) {
+//     // S'il y a un message d'erreur affiché et que le champ
+//     // est valide, on retire l'erreur
+//     error.innerHTML = ""; // On réinitialise le contenu
+//     error.className = "error"; // On réinitialise l'état visuel du message
+//   }
+// }, false);
+// form.addEventListener("submit", function (event) {
+//   // Chaque fois que l'utilisateur tente d'envoyer les données
+//   // on vérifie que le champ email est valide.
+//   if (!email.validity.valid) {
+
+//     // S'il est invalide, on affiche un message d'erreur personnalisé
+//     error.innerHTML = "J'attends une adresse e-mail correcte, mon cher&nbsp;!";
+//     error.className = "error active";
+//     // Et on empêche l'envoi des données du formulaire
+//     event.preventDefault();
+//   }
+// }, false);
+
+
+
 function CircleArea(r) {
     return Math.PI * (r * r);
  }
