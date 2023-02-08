46be425b55fdc42ee383deb50c31a091ae2a3cb8
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-08 01:40:53 +0100

    travail sur le résultat en js

--- cercle2.js
+++ cercle2.js
@@ -1,10 +1,13 @@
-document.write("<form action='' method='get'><label for='number'>Rayon :</label><input type='number' id='number' name='name' size='10'></form>")
-
+var r = document.write("<form action='' method='get'><label for='number'>Rayon :</label><input class='r' type='number' id='number' name='number' size='10'></form>");
 
 function CircleArea(r) {
     return Math.PI * (r * r);
  }
-  
+
+ document.write("L'aire d'un cercle de rayon de "+r+" cm est " +CircleArea(1) +"cm2<BR>");
+
+ document.write("<br>&nbsp;</br>");
+ 
  document.write("L'aire d'un cercle de rayon de 1 cm est " +CircleArea(1) +"cm2<BR>");
  document.write("L'aire d'un cercle de rayon de 5 cm est " +CircleArea(5) +"cm2<BR>");
  document.write("L'aire d'un cercle de rayon de 8 cm est " +CircleArea(8) +"cm2<BR>");
